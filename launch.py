ddb3efc4278d8f050141853e16ef19651f34e1a9
Jim <dermoth@uga.edu>
2018-08-14 17:47:28 -0400

    Zone 8 path through the RBZ

--- launch.py
+++ launch.py
@@ -26,19 +26,19 @@ print a
 GammaX = 2*math.pi/(a*math.sqrt(3))
 #KxMax = 2*math.pi/(3*a)
 
-print "Zone9"
+print "Zone10"
 print "X to Gamma"
-k[0] = -GammaX/2
+k[0] = GammaX/2
 k[1] = -GammaX*math.sqrt(3)/2
 
 step = GammaX/20
 for i in range(0,20):
     runsim(k)
-    k[0] = k[0] + step/2
+    k[0] = k[0] - step/2
     k[1] = k[1] + step*math.sqrt(3)/2
     k[4] +=1.0
     print k
-    print "Zone9"
+    print "Zone10"
 
 
 
@@ -54,7 +54,7 @@ for i in range(0,20):
     k[1] = k[1] - step
     k[4] +=1.0
     print k
-    print "Zone9"
+    print "Zone10"
 
 print "X to J"
 XJ = GammaX/math.sqrt(3)
@@ -62,10 +62,10 @@ step = XJ/20
 
 for i in range(0, 20):
     runsim(k)
-    k[0] = k[0] - step*math.sqrt(3)/2
+    k[0] = k[0] + step*math.sqrt(3)/2
     k[1] = k[1] + step/2
     #k[0] = 2.0*math.pi/(math.sqrt(3.0)*a)*(i - 1)/20.0
     #k[1] = 2.0*math.pi/(3.0*a)*(i - 1)/20.0
     print k
-    print "Zone9"
+    print "Zone10"
     k[4] +=1.0
